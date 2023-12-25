d9a806a580144eff0623a84a18caa29a9ad7ca1d
Charles Lambret <charleslambretpro@gmail.com>
2023-12-25 16:49:00 +0100

    itneraction entre le tableau & le graph

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ function initialize() {
 }
 
 function setupSelectors() {
-    // Define sample sizes here
     var sampleSizes = [5, 10, 20, 50];
     formSampleSize = d3.select("#top").append("form").attr("class", "sample-size");
     formSampleSize.selectAll("label")
@@ -27,7 +26,6 @@ function setupSelectors() {
         .property("checked", (d, i) => i === 0) // default check the first option
         .on("change", updateVisualizations);
     
-    // Year Selector
     selectAnnee = d3.select("#top").append("select")
         .attr("id", "choix_annee")
         .on("change", updateVisualizations);
@@ -38,7 +36,6 @@ function setupSelectors() {
         .attr("value", d => d)
         .text(d => d === "All" ? "All Years" : d);
 
-    // Region Selector
     selectRegion = d3.select("#top").append("select")
         .attr("id", "choix_region")
         .on("change", updateVisualizations);
@@ -49,10 +46,8 @@ function setupSelectors() {
         .attr("value", d => d)
         .text(d => d);
 
-    // Table body
     tbody = d3.select("#top").append("table").append("tbody");
 
-    // FLOP Checkbox
     var form = d3.select("#top").append("form").attr("class", "choix");
     var fieldset = form.append("fieldset");
     fieldset.append("legend").text("Choisir si vous souhaitez un FLOP");
@@ -61,12 +56,11 @@ function setupSelectors() {
         .append("input")
         .attr("type", "checkbox")
         .attr("name", "flop")
-        .property("checked", false) // Set to true for TOP by default
+        .property("checked", false) 
         .on("change", updateVisualizations);
 }
 
 function setupTableau() {
-    // Create table headers
     var table = d3.select("#top").append("table");
     table.append("thead")
         .append("tr")
@@ -80,28 +74,23 @@ function setupTableau() {
 }
 
 function setupNuagedePoints() {
-    // Set up color scale and SVG dimensions
     colorScale = d3.scaleSequential(d3.interpolateRdYlGn).domain([1, 100]);
     var margin = {top: 10, right: 30, bottom: 30, left: 40},
     width = window.innerWidth * 0.90 - margin.left - margin.right, 
     height = window.innerHeight * 0.90 - margin.top - margin.bottom;
 
-    // Create SVG element
     svg = d3.select("#nuage").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // Define scales
     xScale = d3.scaleLog().domain([1, 100000]).range([0, width]);
     yScale = d3.scaleLog().domain([0.1, 100]).range([height, 0]);
 
-    // Add axes to the SVG
     svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(xScale));
     svg.append("g").call(d3.axisLeft(yScale));
 
-    // Add axes labels
     svg.append("text")
         .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")")
         .style("text-anchor", "middle")
@@ -117,7 +106,6 @@ function setupNuagedePoints() {
 }
 
 function setupTooltip() {
-    // Set up a tooltip div
     tooltipDiv = d3.select("body").append("div")
         .attr("id", "tooltip")
         .style("opacity", 0)
@@ -137,13 +125,10 @@ function updateTableau() {
     var isFlop = checkboxFlop.node().checked;
     var selectedSize = parseInt(d3.select('input[name="sampleSize"]:checked').node().value);
 
-    // Filter and aggregate data based on selections
     var filteredData = aggregateAndFilterData(selectedYear, selectedRegion, isFlop);
 
-    // Slice data to respect the selected sample size
     filteredData = filteredData.slice(0, selectedSize);
 
-    // Update table rows
     var rows = tbody.selectAll("tr").data(filteredData);
     rows.exit().remove();
     rows.enter()
@@ -158,17 +143,13 @@ function updateNuagedePoints() {
     var isFlop = checkboxFlop.node().checked;
     var selectedSize = parseInt(d3.select('input[name="sampleSize"]:checked').node().value);
 
-    // Filter and aggregate data based on selections
     var filteredData = aggregateAndFilterData(selectedYear, selectedRegion, isFlop);
 
-    // Slice data to respect the selected sample size
     filteredData = filteredData.slice(0, selectedSize);
 
-    // Update domains for the scales
     xScale.domain([d3.min(filteredData, d => d.Documents) || 1, d3.max(filteredData, d => d.Documents) || 100]);
     yScale.domain([d3.min(filteredData, d => d.Citations) || 0.1, d3.max(filteredData, d => d.Citations) || 100]);
 
-    // Update the circles in the scatter plot
     var circles = svg.selectAll("circle").data(filteredData);
     circles.enter()
         .append("circle")
@@ -176,7 +157,6 @@ function updateNuagedePoints() {
         .on("mouseover", function(event, d) {
             d3.select(this).attr("stroke", "black").attr("stroke-width", 2);
             
-            // Update tooltip content
             d3.select("#tooltip")
                 .html(
                     `<strong>Country:</strong> ${d.Country}<br>` +
@@ -204,41 +184,34 @@ function updateNuagedePoints() {
 }
 
 function aggregateAndFilterData(selectedYear, selectedRegion, isFlop) {
-    // Filter data based on the selected region
     var filteredData = data.filter(d => selectedRegion === "All" || d.Region === selectedRegion);
 
     if (selectedYear !== "All") {
-        // Filter data based on the selected year
         filteredData = filteredData.filter(d => d.Year.toString() === selectedYear);
     } else {
-        // Aggregate data if "All" years are selected
         filteredData = aggregateDataAcrossYears(filteredData);
     }
 
-    // Sort the filtered data based on H-index for top/flop functionality
     filteredData.sort(isFlop ? (a, b) => a.Hindex - b.Hindex : (a, b) => b.Hindex - a.Hindex); // Reversing the flop condition
     return filteredData;
 }
 
 function aggregateDataAcrossYears(data) {
-    // Aggregate data across all years
     let aggregatedData = d3.rollup(data, 
         (v) => ({
             Documents: d3.sum(v, d => d.Documents),
             Citations: d3.sum(v, d => d.Citations),
             Hindex: d3.max(v, d => d.Hindex),
-            Rank: d3.mean(v, d => d.Rank) // Assuming rank needs to be averaged
+            Rank: d3.mean(v, d => d.Rank) 
         }), 
-        d => d.Country); // Group by Country
+        d => d.Country); 
 
-    // Convert the Map returned by d3.rollup to a structure similar to the original data
     return Array.from(aggregatedData, ([Country, values]) => ({
         Country: Country,
         Documents: values.Documents,
         Citations: values.Citations,
         Hindex: values.Hindex,
         Rank: values.Rank,
-        // If Region is important and consistent for a country, derive it from the original data (assumes first match is representative)
         Region: data.find(d => d.Country === Country)?.Region || "Unknown"
     }));
 }
